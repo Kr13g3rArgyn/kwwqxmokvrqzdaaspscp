77ca9db93acde0c58321f3af486576235ec6d526
Elnaz <almasuly.elnaz@gmail.com>
2023-10-24 19:20:01 +0600

    init commit

---
+++ src/main/java/com/example/kwwqxmokvrqzdaaspscp/KwwqxmokvrqzdaaspscpApplication.java
@@ -0,0 +1,13 @@
+package com.example.kwwqxmokvrqzdaaspscp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwwqxmokvrqzdaaspscpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KwwqxmokvrqzdaaspscpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kwwqxmokvrqzdaaspscp/KwwqxmokvrqzdaaspscpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kwwqxmokvrqzdaaspscp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwwqxmokvrqzdaaspscpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
