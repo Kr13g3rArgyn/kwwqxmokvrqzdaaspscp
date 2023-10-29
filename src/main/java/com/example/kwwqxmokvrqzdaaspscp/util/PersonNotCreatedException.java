f4351f002b50feafd8915481cf1bd82076725c8d
Elnaz <almasuly.elnaz@gmail.com>
2023-10-30 01:55:55 +0600

    elnaz did dto

---
+++ src/main/java/com/example/kwwqxmokvrqzdaaspscp/util/PersonNotCreatedException.java
@@ -0,0 +1,7 @@
+package com.example.kwwqxmokvrqzdaaspscp.util;
+
+public class PersonNotCreatedException extends RuntimeException {
+    public PersonNotCreatedException(String msg){
+        super(msg);
+    }
+}
---
+++ src/main/java/com/example/kwwqxmokvrqzdaaspscp/util/PersonNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.kwwqxmokvrqzdaaspscp.util;
+
+public class PersonNotFoundException extends RuntimeException{
+    public PersonNotFoundException(String msg){
+        super(msg);
+    }
+}
