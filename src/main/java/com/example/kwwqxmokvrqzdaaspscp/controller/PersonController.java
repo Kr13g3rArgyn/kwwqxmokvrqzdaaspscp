25b1492cdc0bdc9dc1583f7ab38706336ccd86c8
Elnaz <almasuly.elnaz@gmail.com>
2023-10-30 11:19:12 +0600

    krieger

--- src/main/java/com/example/kwwqxmokvrqzdaaspscp/controller/PersonController.java
+++ src/main/java/com/example/kwwqxmokvrqzdaaspscp/controller/PersonController.java
@@ -2,10 +2,14 @@ package com.example.kwwqxmokvrqzdaaspscp.controller;
 import com.example.kwwqxmokvrqzdaaspscp.dto.PersonDTO;
 import com.example.kwwqxmokvrqzdaaspscp.entity.Person;
 import com.example.kwwqxmokvrqzdaaspscp.service.PersonService;
+import com.example.kwwqxmokvrqzdaaspscp.util.PersonNotCreatedException;
+import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,7 +20,7 @@ public class PersonController {
     private final ModelMapper mapper;
     private final PersonService personService;
 
-    public PersonController(PersonService personService, ModelMapper mapper) {
+        public PersonController(PersonService personService, ModelMapper mapper) {
         this.personService = personService;
         this.mapper = mapper;
     }
@@ -25,7 +29,35 @@ public class PersonController {
     private List<PersonDTO> getAll(){
         return personService.getAll().stream().map(this::convertToPeopleDTO).collect(Collectors.toList());
     }
+    @GetMapping("/{id}")
+    private Person getOneDude(int id){
+        return personService.findOne(id);
+    }
 
+    @DeleteMapping("/delete/{id}")
+    private void deletePerson(int id){
+        personService.deletePerson(id);
+    }
+
+    @PostMapping("/create")
+    private ResponseEntity<HttpStatus> createPerson(@RequestBody @Valid PersonDTO personDTO, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            StringBuilder msg = new StringBuilder();
+            List<FieldError> errors = bindingResult.getFieldErrors();
+            for (FieldError fieldError : errors){
+                msg.append(fieldError.getField())
+                        .append(" - ").append(fieldError.getDefaultMessage())
+                        .append(";");
+            }
+            throw new PersonNotCreatedException(msg.toString());
+        }
+        personService.create(convertToPerson(personDTO));
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+    @PutMapping("/update/{id}")
+    private Person updatePerson(@PathVariable int id, @RequestBody Person person){
+               return personService.updatePeople(id, person);
+    }
 
 
     private Person convertToPerson(PersonDTO peopleDTO) {
---
+++ src/main/java/com/example/kwwqxmokvrqzdaaspscp/entity/Filter.java
@@ -0,0 +1,9 @@
+package com.example.kwwqxmokvrqzdaaspscp.entity;
+
+import jakarta.persistence.Entity;
+
+
+public class Filter {
+    private int limit;
+    private int offset;
+}
